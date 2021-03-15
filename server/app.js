26c893d89c95ebf337bc9784aa6742845a1d2155
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-03-15 11:46:09 -0400

    Changed PORT

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import YoutubeScraper from './middleware/YoutubeScraper.js';
 import express from 'express';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const randomVideosRouter = express.Router();
 
 app.use('/search', randomVideosRouter)
